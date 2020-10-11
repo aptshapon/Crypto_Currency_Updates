fceb9ea918629ae3478a307b574c28ca8621e0ab
Shapon Sheikh <aptshapon@hotmail.com>
2020-10-11 14:08:45 +0600

    Heroku setup

--- project/settings.py
+++ project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'z&kimqv$5w^*mp8d&e#ge1v_xi-+q8n3-n+61-rd2=1t7@ib*6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['crypto-currency-news-bd.herokuapp.com']
 
 
 # Application definition
@@ -118,4 +118,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
